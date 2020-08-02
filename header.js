c0e5eb81ae1fa7b8fa1fbb6d0fc1fd7b994fc88b
Jari Miers <jarimiers@MacBook-Pro-van-Jari.local>
2020-08-02 12:15:16 +0200

    final setup

--- header.js
+++ header.js
@@ -1,6 +1,28 @@
 'use strict';
 
-// import context from "./context/context.js";
+import context from "./context/context.js";
 // import * as Utils from "./context/utils.js";
 
-console.log("dit is een test.")
+
+const width = context.canvas.width;
+const height = context.canvas.height;
+
+let movingMouse = true;
+
+drawBackground();
+
+
+document.onmousemove = moveMouse;
+
+function moveMouse(e) {
+
+
+}
+
+function drawBackground() {
+
+    context.fillStyle = "black"
+    context.font = "bold 60pt Arial";
+    context.fillText("Jari Miers", width / 2 - 200, 250);
+
+}
