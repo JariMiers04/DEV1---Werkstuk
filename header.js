b256c9eadf6762e7352b3ff7c226483352f82677
Jari Miers <jarimiers@MacBook-Pro-van-Jari.local>
2020-08-05 14:39:17 +0200

    amplitude update

--- header.js
+++ header.js
@@ -1,46 +1,61 @@
-'use strict';
+// http://www.javascriptkit.com/javatutors/requestanimationframe.shtml#:~:text=Slowing%20down%20or%20cancelling%20requestAnimationFrame()&text=If%20your%20animation%20requires%20a,calling%20requestAnimationFrame%20inside%20setTimeout()%20.
+// link how to delay requestAnimationFrame();
 
+"use strict";
 import context from "./context/context.js";
 import * as Utils from "./context/utils.js";
-
 
 const width = context.canvas.width;
 const height = context.canvas.height;
 
-let movingMouse = true;
+
+let fps = 12;
 let space = 200;
+let counter = 10;
+
+// xPosMouse for changing the color
+let xPosMouse = [];
 
 
-// XposMouse for changing the color
-let XposMouse = [];
-// YposMouse for changing the amplitude
-let YposMouse = [];
 
-drawBackground();
-
+drawBackgroundAnimation();
 
 document.onmousemove = moveMouse;
 
 function moveMouse(e) {
-
-
+    let mouse = {
+        y: e.pageY
+    };
+    xPosMouse.push(mouse);
+    for (let i = 0; i < xPosMouse.length; i++) {
+        let mouseInfoArray = xPosMouse[i];
+    }
+    if (e.pageY > 600 && e.pageY > height && counter >= 3) {
+        counter -= 0.2;
+    } else if (e.pageY < 600 && e.pageY > 0 && counter <= 15) {
+        counter += 0.2;
+    }
+    console.log(counter);
 }
 
-function drawBackground() {
-    context.fillStyle = 'black';
+function drawBackgroundAnimation() {
+    context.fillStyle = "black";
     context.fillRect(0, 0, width, height);
-    let Yrandom = Utils.randomNumber(2, 10);
-
     for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < Yrandom; j++) {
-            context.fillStyle = 'red';
-            context.fillRect((0 + space) + i * space, (height - 40) - j * 40, 100, 20);
+        let Yrandom = Utils.randomNumber(1, counter);
+        context.fillStyle = 'red';
+        for (let j = 0; j < 20; j++) {
+            context.fillRect(0 + space + i * space, height - 40 - j * 40, 100, 20);
+            if (j === Yrandom) {
+                break;
+            }
         }
     }
 
-    context.fillStyle = "white"
+    context.fillStyle = "white";
     context.font = "bold 60pt Arial";
     context.fillText("Jari Miers", width / 2 - 200, height / 2);
-    requestAnimationFrame(drawBackground);
-
+    setTimeout(() => {
+        requestAnimationFrame(drawBackgroundAnimation);
+    }, 1000 / fps);
 }
